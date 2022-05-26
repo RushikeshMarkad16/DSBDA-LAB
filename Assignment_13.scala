7f0d1caa97c9eddcf4bf8a0e5bae637ad4d412e7
Rushikesh Markad <60238321+RushikeshMarkad16@users.noreply.github.com>
2022-05-26 18:38:44 +0530

    Create Assignment_13.scala

---
+++ Assignment_13.scala
@@ -0,0 +1,64 @@
+#Code
+
+class Calculator 
+{
+  def +(a: Int, b: Int): Int = a+b
+  
+  def -(a: Int, b: Int): Int = a-b
+  
+  def *(a: Int, b: Int): Long = a*b
+  
+  def /(a: Int, b: Int): Int = 
+  {
+    require(b != 0, "denominator can not be 0")    
+    a/b
+  }
+}
+ 
+object Calendar
+{
+  def main(args: Array[String]) =
+  {
+    val calc = new Calculator()
+    
+    println("Addition: " + calc.+(10, 2))
+    println("Subtraction: " + calc.-(10, 2))
+    println("Multiplication: " + calc.*(10, 2))
+    println("Division: " + calc./(10, 2))
+ 
+    //println("Division: " + calc./(10, 0))
+  }
+}
+
+
+
+
+#Execution Steps
+"""
+## One Terminal 1 and run following command
+nc -lk 9999
+
+## Open Terminal 2 and run following commands line by line
+
+spark-shell
+
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.SparkSession
+import spark.implicits._
+
+val spark = SparkSession.builder.appName("StructuredNetworkWordCount").getOrCreate()
+  
+
+val lines = spark.readStream.format("socket").option("host", "spark").option("port", 9999).load()
+val words = lines.as[String].flatMap(_.split(" "))
+val wordCounts = words.groupBy("value").count()
+
+val query = wordCounts.writeStream.outputMode("complete").format("console").start()
+
+## query.awaitTermination() //Most probably Not be needed
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+"""
